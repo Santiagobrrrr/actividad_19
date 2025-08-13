23a9bb83662c6f6bea3b10e1b0c6487451354a07
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-08-13 15:46:48 -0600

    MENÚ SIN FUNCIONES

---
+++ actividad_19.py
@@ -0,0 +1,35 @@
+print(f"-- Menú de Galletas --")
+print(f"1. Registrar galleta básica")
+print(f"2. Registrar galleta con chispas")
+print(f"3. Registrar galleta rellena")
+print(f"4. Lista de galletas")
+print(f"5. Buscar galleta")
+print(f"6. Eliminar galleta")
+print(f"7. Salir")
+
+option_user = input(f"Ingrese la opción que desea realizar: ")
+
+match option_user:
+    case "1":
+        print(f"- Registrar galleta básica -")
+
+    case "2":
+        print(f"- Registrar galleta con chispas -")
+
+    case "3":
+        print(f"- Registrar galleta rellena -")
+
+    case "4":
+        print(f"- Lista de galletas -")
+
+    case "5":
+        print(f"- Buscar galleta por nombre -")
+
+    case "6":
+        print(f"- Eliminar galleta -")
+
+    case "7":
+        print(f"Saliendo del programa ...")
+
+    case _:
+        print(f"Error: ingreso una opción inválida, intente de nuevo.")
